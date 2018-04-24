db8e8039967f76a26ecf163a4936d304510d1e96
Amiri Irish <amiri.irish13@gmail.com>
2018-04-23 21:37:17 -0700

    Started writing the vector class. Wrote all the declarations, and two constructors.

---
+++ PIC10C_HW1_ATTEMPT1/VectorTemplatedClass.h
@@ -0,0 +1,51 @@
+#ifndef __VECTOR_CLASS__
+#define __VECTOR_CLASS__
+
+#include <iostream>
+#include <memory>
+
+namespace Pic10c {
+    template <typename T>
+    class vector{
+    private:
+        size_t sz;
+        size_t cap;
+        std::unique_ptr<T> uniquePointer;
+        static const int initinal_cap = 10;
+    public:
+        vector() : sz(0), cap(static_cast<size_t>(initinal_cap)) {
+            uniquePointer.reset(new T[initinal_cap]);
+        }
+        
+        vector(const vector& copy) : sz(copy.sz), cap(copy.cap) {
+            uniquePointer.reset(new T[cap]);
+            for (size_t i = 0; i < sz; ++i){
+                uniquePointer[i] = copy.uniquePointer[i];
+            }
+        }
+        
+        vector(vector&& move);
+        vector& operator=(const vector& copy);
+        bool empty() const;
+        size_t size() const;
+        size_t capacity() const;
+        T front() const;
+        T back() const;
+        T at(size_t index) const;
+        T& operator[](size_t index);
+        T operator[](size_t index) const;
+        void data_dump_to(std::ostream& out) const;
+        void data_dump() const;
+        void push_back(const T& input);
+        void pop_back();
+        void deleteAt(size_t index);
+    };
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const Pic10c::vector<T>& vec);
+
+template <typename T>
+void print_vector(const Pic10c::vector<T>& vec);
+
+#endif
---
+++ PIC10C_HW1_ATTEMPT1/vector_main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include "VectorTemplatedClass.h"
+
+int main() {
+    return 0;
+}
