ffbceae6544ef3fbecd104dc984beffecb4c9e8d
Amiri Irish <amiri.irish13@gmail.com>
2018-04-25 13:59:29 -0700

    Commented some of the functions and cleared main so I can merge it with the experimental_1 branch content

--- PIC10C_HW1_ATTEMPT1/VectorTemplatedClass.h
+++ PIC10C_HW1_ATTEMPT1/VectorTemplatedClass.h
@@ -5,18 +5,41 @@
 #include <memory>
 
 namespace Pic10c {
+    
+    /**
+     The vector class stores objects of any type
+     @tparam T , the data type the vector stores
+     */
     template <typename T>
     class vector{
+    
     private:
-        size_t sz;
-        size_t cap;
-        std::unique_ptr<T[]> uniquePointer;
-        static const int initinal_cap = 10;
+        size_t sz; // size of the vector
+        
+        size_t cap; // capacity of the vector
+        
+        std::unique_ptr<T[]> uniquePointer; // unique_ptr to an array that stores type T
+        
+        static const int initinal_cap = 10; // initial capacity set to 10
+        
     public:
+        /**
+         vector constructor:
+         creates a vector having capacity of 10 with default inititalized elements
+         */
         vector() : sz(0), cap(static_cast<size_t>(initinal_cap)) {
             uniquePointer.reset(new T[initinal_cap]);
         }
         
+        /**
+         default destructor
+         */
+        ~vector() {}
+        
+        /**
+         copy constructor:
+         @param copy , the vector to be copied
+         */
         vector(const vector& copy) : sz(copy.sz), cap(copy.cap) {
             uniquePointer.reset(new T[cap]);
             for (size_t i = 0; i < sz; ++i){
@@ -24,9 +47,19 @@ namespace Pic10c {
             }
         }
         
+        /**
+         move constructor
+         @param vec , vector to be moved
+         */
         vector(vector&& vec) : sz(vec.sz), cap(vec.cap) {
             uniquePointer(std::move(vec.uniquePointer));
         }
+        
+        /**
+         copy and move assignment operator
+         @prama vec , the vector to be copied/moved
+         @return the copied/moved vector
+         */
         vector& operator=(vector vec) {
             std::swap(sz, vec.sz);
             std::swap(cap, vec.cap);
@@ -34,35 +67,40 @@ namespace Pic10c {
             vec.uniquePointer.reset();
             return *this;
         }
-        /*bool empty() const;
+        bool empty() const;
         size_t size() const;
         size_t capacity() const;
         T front() const;
-        T back() const;*/
+        T back() const;
         T at(size_t index) const {
             return uniquePointer[index];
         }
-        /*T& operator[](size_t index);
+        T& operator[](size_t index);
         T operator[](size_t index) const;
         void data_dump_to(std::ostream& out) const;
-        void data_dump() const;*/
-        void push_back(const T& input) {
-            if (sz == cap){
-                std::cout << "hi";
-            }
-            else {
-                ++sz;
-                uniquePointer[sz-1] = input;
-            }
+        void dump_data() const;
+        void push_back(const T& input){
+            
         }
-        /*void pop_back();
-        void deleteAt(size_t index);*/
+        void pop_back();
+        //void deleteAt(size_t index);
     };
 }
 
+/**
+ this function overloads the operator << to be able to print the vector contents
+ @tparam T , data type the vector stores
+ @param out , output stream
+ @param vec , the vector to be displayed to the console
+ */
 template <typename T>
 std::ostream& operator<<(std::ostream& out, const Pic10c::vector<T>& vec);
 
+/**
+ this function prints the vector to the console
+ @tparam T , data type the vector stores
+ @param vec , the vector to be printed
+ */
 template <typename T>
 void print_vector(const Pic10c::vector<T>& vec);
 
--- PIC10C_HW1_ATTEMPT1/vector_main.cpp
+++ PIC10C_HW1_ATTEMPT1/vector_main.cpp
@@ -3,22 +3,6 @@
 #include "VectorTemplatedClass.h"
 
 int main() {
-    Pic10c::vector<int> v;
-    Pic10c::vector<int> u;
-    v.push_back(3);
-    v.push_back(4);
-    for (size_t i = 0; i < 2; ++i){
-        std::cout << v.at(i) << " ";
-    }
-    std::cout << std::endl;
-    u.push_back(2);
-    for (size_t i = 0; i < 1; ++i){
-        std::cout << u.at(i) << " ";
-    }
-    std::cout << std::endl;
-    u = v;
-    for (size_t i = 0; i < 2; ++i){
-        std::cout << u.at(i) <<  " ";
-    }
+   
     return 0;
 }
